272db97b7415ede1fdae1b3d45ff42596557aa37
Diaspar <the.diaspar@gmail.com>
2018-08-24 23:43:25 -0400

    changes

--- assets/javascript/movieapp.js
+++ assets/javascript/movieapp.js
@@ -1,19 +1,6 @@
 
    $(document).ready(function () {
 
-        // Initialize Firebase
-        var config = {
-       apiKey: "AIzaSyASQ8jBTorfDSw2R15DJuRl8qXgnxXOAh0",
-       authDomain: "moodfuse-afb60.firebaseapp.com",
-       databaseURL: "https://moodfuse-afb60.firebaseio.com",
-       projectId: "moodfuse-afb60",
-       storageBucket: "moodfuse-afb60.appspot.com",
-       messagingSenderId: "163903846814"
-       };
-      firebase.initializeApp(config);
-
-      var database = firebase.database();
-
 
     // Base url for main API call
     var apiBaseURL = "https://api.themoviedb.org/3/configuration?api_key=56f6c2183bfbb1abccc25877c84a0b7e";
@@ -119,51 +106,6 @@
 
 
 
-   $(".review-submit").on("click", function(event){
-    event.preventDefault();
-    // Grabs user input
-    var userEmail = $("#InputEmail").val().trim();
-    var userReview = $("#ReviewTextarea").val().trim();
-
-    // Pushing to database
-database.ref().push({
-userEmail: userEmail,
-userReview: userReview
-});
-
-console.log(userEmail);
-console.log(userReview);
-
-// Clears all of the text-boxes
-$("#InputEmail").val("");
-$("#ReviewTextarea").val("");
-
-});
-// Create Firebase event for adding user email and review to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
-console.log(childSnapshot.val());
-
-// Store everything into a variable.
-var newuserEmail = childSnapshot.val().userEmail;
-var newuserReview = childSnapshot.val().userReview;
-
-
-// User Info
-console.log(newuserEmail);
-console.log(newuserReview);
-
-
-
-// Create the new row
-var newRow = $("<tr>").append(
-  $("<td>").text(newuserEmail),
-  $("<td>").text(newuserReview),
-  
-);
-
-// Append the new row to the table
-$("#review-table > tbody").append(newRow);
-});
 
      });
 
